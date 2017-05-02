3219dfb57491e8b96e349335e232175a3c9bfd08
Dasiys <yeweimi@iCloud.com>
2017-05-02 16:18:57 +0800

    声明

--- Identity/Infrastructure/Manager/IdentityHelpers.cs
+++ Identity/Infrastructure/Manager/IdentityHelpers.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web;
@@ -15,6 +17,19 @@ namespace Infrastructure.Manager
         {
             AppUserManager mgr = HttpContext.Current.GetOwinContext().GetUserManager<AppUserManager>();
             return new MvcHtmlString(mgr.FindByIdAsync(id).Result.UserName);
+        }
+
+        public static MvcHtmlString ClaimType(this HtmlHelper html,string claimType)
+        {
+            FieldInfo[] fields = typeof(ClaimTypes).GetFields();
+            foreach(FieldInfo field in fields)
+            {
+                if (field.GetValue(null).ToString() == claimType)
+                {
+                    return new MvcHtmlString(claimType);
+                }
+            }
+            return new MvcHtmlString(string.Format("{0}", claimType.Split('/', '.').Last()));
         }
     }
 }
