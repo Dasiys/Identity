338d3922c3994ad8f7001c1e8e7f063506d9a7cf
Dasiys <yeweimi@iCloud.com>
2017-05-02 10:18:15 +0800

    角色

---
+++ Identity/Identity/Controllers/RoleAdminController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Infrastructure.Manager;
+using Microsoft.AspNet.Identity.Owin;
+using System.ComponentModel.DataAnnotations;
+using Domain.Model;
+using System.Threading.Tasks;
+
+namespace Identity.Controllers
+{
+    public class RoleAdminController : Controller
+    {
+        // GET: RoleAdmin
+        public ActionResult Index()
+        {
+            return View(RoleManager.Roles);
+        }
+
+        private AppUserManager UserManager
+        {
+            get
+            {
+                return HttpContext.GetOwinContext().GetUserManager<AppUserManager>();
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create([Required]string name)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityResult result = await RoleManager.CreateAsync(new AppRole(name));
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    AddErrorsFromResult(result);
+                }
+            }
+            return View(name);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Delete(string id)
+        {
+            AppRole role = await RoleManager.FindByIdAsync(id);
+            if (role != null)
+            {
+                IdentityResult result = await RoleManager.DeleteAsync(role);
+                if (result.Succeeded)
+                {
+                    return RedirectToActionPermanent("Index");
+                }
+                else
+                {
+                    return View("Error", result.Errors);
+                }
+
+            }
+            else
+            {
+                return View("Error", new string[] { "Role Not Found" });
+            }
+        }
+
+        private void AddErrorsFromResult(IdentityResult result)
+        {
+            foreach(string error in result.Errors)
+            {
+                ModelState.AddModelError("", error);
+            }
+        }
+
+        private AppRoleManager RoleManager
+        {
+            get
+            {
+                return HttpContext.GetOwinContext().GetUserManager<AppRoleManager>();
+            }
+        }
+    }
+}
