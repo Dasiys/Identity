48e44b0705e7c508bc0317f509fc691810b5f8b8
Dasiys <yeweimi@iCloud.com>
2017-05-03 15:26:46 +0800

    增加webApi

---
+++ Identity/Identity/Controllers/ImageController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+
+namespace Identity.Controllers
+{
+    public class ImageController : ApiController
+    {
+        [System.Web.Http.HttpPost]
+        public Task<Hashtable> Post()
+        {
+            // 检查是否是 multipart/form-data 
+            if (!Request.Content.IsMimeMultipartContent("form-data"))
+                throw new HttpResponseException(HttpStatusCode.UnsupportedMediaType);
+            //文件保存目录路径 
+            //string SaveTempPath = "~/SayPlaces/" + "/SayPic/SayPicTemp/";
+            //String dirTempPath = HttpContext.Current.Server.MapPath(SaveTempPath);
+            // 设置上传目录 
+            var provider = new MultipartFormDataStreamProvider(@"F:\\WebApi\\UpLoad");
+            //var queryp = Request.GetQueryNameValuePairs();//获得查询字符串的键值集合 
+            var task = Request.Content.ReadAsMultipartAsync(provider).
+                ContinueWith<Hashtable>(o =>
+                {
+                    Hashtable hash = new Hashtable();
+                    hash["error"] = 1;
+                    hash["errmsg"] = "上传出错";
+                    var file = provider.FileData[0];//provider.FormData 
+                    string orfilename = file.Headers.ContentDisposition.FileName.TrimStart('"').TrimEnd('"');
+                    FileInfo fileinfo = new FileInfo(file.LocalFileName);
+                    //最大文件大小 
+                    int maxSize = 10000000;
+                    if (fileinfo.Length <= 0)
+                    {
+                        hash["error"] = 1;
+                        hash["errmsg"] = "请选择上传文件。";
+                    }
+                    else if (fileinfo.Length > maxSize)
+                    {
+                        hash["error"] = 1;
+                        hash["errmsg"] = "上传文件大小超过限制。";
+                    }
+                    else
+                    {
+                        string fileExt = orfilename.Substring(orfilename.LastIndexOf('.'));
+                        //定义允许上传的文件扩展名 
+                        String fileTypes = "gif,jpg,jpeg,png,bmp";
+                        if (String.IsNullOrEmpty(fileExt) || Array.IndexOf(fileTypes.Split(','), fileExt.Substring(1).ToLower()) == -1)
+                        {
+                            hash["error"] = 1;
+                            hash["errmsg"] = "上传文件扩展名是不允许的扩展名。";
+                        }
+                        else
+                        {
+                            String ymd = DateTime.Now.ToString("yyyyMMdd", System.Globalization.DateTimeFormatInfo.InvariantInfo);
+                            String newFileName = DateTime.Now.ToString("yyyyMMddHHmmss_ffff", System.Globalization.DateTimeFormatInfo.InvariantInfo);
+                            fileinfo.CopyTo(Path.Combine(@"F:\\WebApi\\UpLoad", newFileName + fileExt), true);
+                            fileinfo.Delete();
+                            hash["error"] = 0;
+                            hash["errmsg"] = "上传成功";
+                        }
+                    }
+                    return hash;
+                });
+            return task;
+        }
+    }
+}
--- Identity/Identity/Global.asax.cs
+++ Identity/Identity/Global.asax.cs
@@ -1,16 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity;
-using System.Data.Entity.Migrations;
-using System.Linq;
-using System.Web;
+﻿using System.Data.Entity;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
-
+using Identity.App_Start;
 using Infrastructure.Database;
-using System.Configuration;
-
+using System.Web;
 namespace Identity
 {
     public class MvcApplication : System.Web.HttpApplication
@@ -19,9 +13,9 @@ namespace Identity
         {
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            System.Web.Http.GlobalConfiguration.Configure(WebApiConfig.Register);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-            Database.SetInitializer(new IdentityDbInit());
         }
     }
 }
