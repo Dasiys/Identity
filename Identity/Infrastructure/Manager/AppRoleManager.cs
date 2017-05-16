392a358c109fd03a1d5b71a7952f00bda29cd975
Dasiys <yeweimi@iCloud.com>
2017-05-16 15:05:02 +0800

    修复Bug

--- Identity/Infrastructure/Manager/AppRoleManager.cs
+++ Identity/Infrastructure/Manager/AppRoleManager.cs
@@ -13,11 +13,11 @@ using Microsoft.Owin;
 
 namespace Infrastructure.Manager
 {
-    public class AppRoleManager:RoleManager<AppRole>,IDisposable
+    public class AppRoleManager : RoleManager<AppRole>, IDisposable
     {
         public AppRoleManager(RoleStore<AppRole> store) : base(store) { }
 
-        public static AppRoleManager Create(IdentityFactoryOptions<AppRoleManager> options,IOwinContext context)
+        public static AppRoleManager Create(IdentityFactoryOptions<AppRoleManager> options, IOwinContext context)
         {
             return new AppRoleManager(new RoleStore<AppRole>(context.Get<AppIdentityDbContext>()));
         }
@@ -28,12 +28,13 @@ namespace Infrastructure.Manager
         /// <param name="role"></param>
         /// <param name="perissionIds"></param>
         /// <param name="context"></param>
-        public void AddRolePermission(AppRole role, string[] perissionIds,AppIdentityDbContext context)
+        public void AddRolePermission(AppRole role, string[] perissionIds, AppIdentityDbContext context)
         {
             if (role != null)
             {
                 role.Permissions.Clear();
-                role.Permissions =  new PermissionManager(context).GetPermission(m => perissionIds.Contains(m.Id.ToString()));
+                if (perissionIds!=null&&perissionIds.Any())
+                    role.Permissions = new PermissionManager(context).GetPermission(m => perissionIds.Contains(m.Id.ToString()));
             }
         }
     }
